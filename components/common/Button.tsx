f5cbcb6a74278e7b330f0b82e93b3ac4289b7aa3
양은지 <eunji124@github.com>
2025-11-19 15:43:41 +0900

    ✨Feat: 공통 버튼 컴포넌트 작업:cva 형태로 변경

--- components/common/Button.tsx
+++ components/common/Button.tsx
@@ -1,51 +1,63 @@
-import React from 'react';
+import { cva, VariantProps } from 'class-variance-authority';
 
-interface ButtonProps {
+const buttonVariants = cva(
+  'shrink transition-colors font-bold text-center rounded-md w-full',
+  {
+    variants: {
+      variant: {
+        primary:
+          'bg-red-50 border border-red-50 text-white hover:bg-white hover:text-red-50 cursor-pointer',
+        secondary:
+          'bg-white border border-red-50 text-red-50 hover:bg-red-50 hover:text-white cursor-pointer',
+      },
+      size: {
+        large: 'max-w-[350px] h-[48px] text-base',
+        medium: 'max-w-[108px] h-[37px] text-sm',
+        small: 'max-w-[82px] h-[32px] font-normal text-xs',
+      },
+      disabled: {
+        true: 'cursor-not-allowed pointer-events-none',
+        false: '',
+      },
+    },
+    compoundVariants: [
+      {
+        disabled: true,
+        class:
+          '!bg-gray-40 !border-gray-40 !text-white hover:!bg-gray-40 hover:!text-white',
+      },
+    ],
+    defaultVariants: {
+      variant: 'primary',
+      size: 'medium',
+      disabled: false,
+    },
+  }
+);
+
+interface ButtonProps extends VariantProps<typeof buttonVariants> {
   /** 버튼에 표시될 텍스트 */
   label: string;
-  /** 버튼 크기: small, medium, large */
-  size?: 'small' | 'medium' | 'large';
-  /** 버튼 스타일: primary / secondary */
-  variant?: 'primary' | 'secondary';
-  /** 버튼 비활성화 여부 */
-  disabled?: boolean;
   /** 버튼 클릭 시 호출될 함수 */
   onClick?: () => void;
+  /** disabled 상태 */
+  disabled?: boolean;
 }
 
-const Button: React.FC<ButtonProps> = ({
+const Button = ({
   label,
-  size = 'medium',
-  variant = 'primary',
-  disabled = false,
   onClick,
-}) => {
-  // 사이즈별 스타일
-  const sizeClasses = {
-    large:
-      'w-full max-w-[350px] h-[48px] font-bold text-base text-center rounded-md',
-    medium:
-      'w-full max-w-[108px] h-[37px] font-bold text-sm text-center rounded-md',
-    small:
-      'w-full max-w-[82px] h-[32px] font-normal text-xs text-center rounded-md',
-  };
-  // 상태별 스타일
-  const variantClasses: Record<NonNullable<ButtonProps['variant']>, string> = {
-    primary:
-      'bg-red-50 border border-red-50 text-white hover:bg-white hover:text-red-50 cursor-pointer',
-    secondary:
-      'bg-white border border-red-50 text-red-50 hover:bg-red-50 hover:text-white cursor-pointer',
-  };
-
-  // disabled 상태 클래스
-  const disabledClasses =
-    'bg-gray-40 text-white cursor-not-allowed pointer-events-none';
-
+  disabled = false,
+  variant,
+  size,
+}: ButtonProps) => {
   return (
     <button
-      className={`${sizeClasses[size]} ${
-        disabled ? disabledClasses : variantClasses[variant]
-      } shrink transition-colors`}
+      className={buttonVariants({
+        variant,
+        size,
+        disabled,
+      })}
       onClick={onClick}
       disabled={disabled}>
       {label}
