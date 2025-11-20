1f89a0537a09fd12eb5bfab3daf01eee4d6debad
황태우 <qqaws4821@naver.com>
2025-11-20 13:42:03 +0900

    🔥Remove : 불필요한 내용 정리

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,5 @@
-import { useState } from "react";
-import AlertModal from "../components/common/alertmodal";
-import ConfirmModal from "../components/common/confirmmodal";
+const Home = () => {
+  return;
+};
 
-export default function Home() {
-  const [confirmOpen, setConfirmOpen] = useState(false);
-  const [alertOpen, setAlertOpen] = useState(false);
-
-  return (
-    <div className="p-10">
-
-      {/* Confirm Modal 버튼 */}
-      <button
-        onClick={() => setConfirmOpen(true)}
-        className="border p-2 bg-blue-200"
-      >
-        Confirm Modal 열기
-      </button>
-
-      <ConfirmModal
-        isOpen={confirmOpen}
-        message="신청을 거절하시겠어요?"
-        onConfirm={() => {
-          alert("확인 클릭됨");
-          setConfirmOpen(false);
-        }}
-        onCancel={() => setConfirmOpen(false)}
-      />
-
-      {/* Alert Modal 버튼 */}
-      <button
-        className="border p-2 mt-4"
-        onClick={() => setAlertOpen(true)}
-      >
-        Alert Modal 열기
-      </button>
-
-      <AlertModal
-        isOpen={alertOpen}
-        message="가게 정보를 먼저 등록해 주세요."
-        onClose={() => setAlertOpen(false)}
-      />
-    </div>
-  );
-}
+export default Home;
