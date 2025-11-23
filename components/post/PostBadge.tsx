f4492e6a0105855525226a012ef4c814756e2447
le2yunji <le2yunji@naver.com>
2025-11-23 23:48:47 +0900

    🐛Fix: 뱃지 색상 표시 오류 해결

--- components/post/PostBadge.tsx
+++ components/post/PostBadge.tsx
@@ -5,11 +5,13 @@ import BadgeArrow from '../icons/BadgeArrow';
 
 const PostBadge = ({ percentage, isGray, getBadgeColor }: BadgeProps) => (
   <>
+    {/* 데스크탑, 태블릿 버전 뱃지 */}
     <span
       className={`${postClasses.badge({ isActive: isGray })} ${getBadgeColor(percentage, true)}`}>
       기존 시급보다 {percentage}%
       <BadgeArrow className={postClasses.badgeArrow()} />
     </span>
+    {/* 모바일 버전 뱃지 */}
     <span
       className={`${postClasses.badgeText({ isActive: isGray })} ${getBadgeColor(percentage, false)}`}>
       기존 시급보다 {percentage}%
