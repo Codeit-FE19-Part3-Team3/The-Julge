b6b24e284b0cb9789b1a9981086123df075df005
jiyun0522 <wnlemforhs24@gmail.com>
2025-12-02 19:32:55 +0900

    ♻️Refactor: NotificationModal에서 리팩토링된 NotificationItem 구조 적용 및 읽음 처리 연동

--- components/common/notification/NotificationModal.tsx
+++ components/common/notification/NotificationModal.tsx
@@ -3,21 +3,20 @@ import { useEffect } from 'react';
 import Image from 'next/image';
 
 import NotificationItem from '@/components/common/notification/NotificationItem';
+import { Notification } from '@/hooks/useNotifications';
 
 interface NotificationModalProps {
   isOpen: boolean;
   onClose: () => void;
-  notifications: Array<{
-    message: string;
-    status: string;
-    time: string;
-  }>;
+  notifications: Array<Notification>;
+  onReadNotification: (notificationId: string) => void;
 }
 
 const NotificationModal = ({
   isOpen,
   onClose,
   notifications,
+  onReadNotification,
 }: NotificationModalProps) => {
   // 스크롤 방지
   useEffect(() => {
@@ -47,11 +46,11 @@ const NotificationModal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="bg-red-10 sm:border-gray-30 fixed inset-0 flex flex-col gap-4 overflow-hidden px-5 py-10 sm:absolute sm:inset-auto sm:top-14 sm:right-8 sm:max-h-[60vh] sm:gap-4 sm:rounded-[10px] sm:border sm:px-5 sm:py-6">
+    <div className="bg-red-10 sm:border-gray-30 fixed inset-0 z-1 flex flex-col gap-4 overflow-hidden px-5 py-10 sm:absolute sm:inset-auto sm:top-14 sm:right-0 sm:max-h-[60vh] sm:w-[368px] sm:gap-4 sm:rounded-[10px] sm:border sm:px-5 sm:py-6">
       {/* 제목 및 닫기 버튼 */}
       <div className="flex w-full items-center justify-between">
         <h2 className="text-xl leading-normal font-bold">
-          알림 {notifications.length}개
+          알림 {notifications.length > 0 && ` ${notifications.length}개`}
         </h2>
         <button onClick={onClose} className="flex h-6 w-6 sm:hidden">
           <Image
@@ -63,18 +62,22 @@ const NotificationModal = ({
         </button>
       </div>
       {/* 알림 영역 */}
-      <div className="flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
-        <div className="flex flex-col items-center justify-center gap-2">
-          {notifications.map((notification, index) => (
-            <NotificationItem
-              key={index}
-              message={notification.message}
-              status={notification.status}
-              time={notification.time}
-            />
-          ))}
+
+      {notifications.length === 0 ? (
+        <div className="text-gray-40 w-full text-lg">알림이 없습니다</div>
+      ) : (
+        <div className="flex-1 overflow-y-auto [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden">
+          <div className="flex flex-col items-center justify-center gap-2">
+            {notifications.map((notification) => (
+              <NotificationItem
+                key={notification.id}
+                notification={notification}
+                onRead={onReadNotification}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
